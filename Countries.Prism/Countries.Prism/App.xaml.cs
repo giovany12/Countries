ce588dcf9069c18e6ab120d0fe054bef381b55a9
giovany12 <36609507+giovany12@users.noreply.github.com>
2019-09-24 16:08:31 -0500

    Login page and login page view model

---
+++ Countries.Prism/Countries.Prism/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using Prism;
+using Prism.Ioc;
+using Countries.Prism.ViewModels;
+using Countries.Prism.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Countries.Prism
+{
+    public partial class App
+    {
+        public App() : this(null) { }
+
+        public App(IPlatformInitializer initializer) : base(initializer) { }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            await NavigationService.NavigateAsync("NavigationPage/LoginPage");
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<LoginPage, LoginPageViewModel>();
+        }
+    }
+}
